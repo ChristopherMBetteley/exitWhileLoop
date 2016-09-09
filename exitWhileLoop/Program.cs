9ed0d10cb5a702427c08a9d6f9610ecb87890d22
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-09 01:01:51 -0400

    first commit

---
+++ exitWhileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exitWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCommand = "exit";
+            string userInput;
+
+            while (true)
+            {
+                userInput = Console.ReadLine();
+                if (userInput.Equals(exitCommand))
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(userInput);
+                }
+            }
+        }
+    }
+}
